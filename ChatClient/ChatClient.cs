ba8b121671aed3eb0e46fbc2d5945a564ea6d74a
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2019-07-30 17:51:51 +0530

    Made the peer-to-peer chat client app

---
+++ ChatClient/ChatClient.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ChatClient
+{
+    class ChatClient
+    {
+        String _myName;
+        int _myPort;
+        String _clientName = "Client";
+        int _clientPort;
+        String _clientIP;
+        IPHostEntry _ipHost;
+        IPAddress _ipAddr;
+
+        public ChatClient()
+        {
+            _ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            _ipAddr = _ipHost.AddressList[1];
+        }
+
+        public void RunNode()
+        {
+            Console.WriteLine("Your Ip Address : " + _ipAddr);
+            Console.Write("Input your Port : ");
+            this._myPort = Int32.Parse(Console.ReadLine());
+            Console.Write("Input your Name :");
+            this._myName = Console.ReadLine();
+
+            Console.Write("Input Remote Host IP : ");
+            this._clientIP = Console.ReadLine();
+            Console.Write("Input Remote Host Port : ");
+            this._clientPort = Int32.Parse(Console.ReadLine());
+
+            Thread sendingThread = new Thread(new ThreadStart(() => this.Send()));
+            sendingThread.Start();
+            Thread listeningThread = new Thread(new ThreadStart(() => this.Listen()));
+            listeningThread.Start();
+        }
+
+        public void Send()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(_clientIP), _clientPort);
+
+            try
+            {
+                var flag = true;
+                while(flag)
+                {
+                    try
+                    {
+                        socket.Connect(endPoint);
+                        flag = false;
+                        Console.WriteLine("Connected!");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Waiting for connection");
+                    }
+                }
+                
+                while (true)
+                {
+                    var message = Console.ReadLine();
+                    var messageToBeSent = Encoding.ASCII.GetBytes(message);
+                    socket.Send(messageToBeSent);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error: Unable to connect to Server!");
+            }
+            //socket.Shutdown(SocketShutdown.Both);
+            //socket.Close();
+
+        }
+
+        public void Listen()
+        {
+            IPEndPoint endPoint = new IPEndPoint(_ipAddr, _myPort);
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(endPoint);
+            listener.Listen(10);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket = listener.Accept();
+                while (true)
+                {
+                    var messageRecieved = new byte[1024];
+                    var byteRecieved = socket.Receive(messageRecieved);
+                    Console.WriteLine(_clientName + ": " + Encoding.ASCII.GetString(messageRecieved, 0, byteRecieved));
+                }            
+            }
+            catch
+            {
+                Console.WriteLine("Error: Unable to connect to Client!");
+            }
+            //socket.Shutdown(SocketShutdown.Both);
+            //socket.Close();
+            
+        }
+    }
+}
---
+++ ChatClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- Chat Program -----");
+            ChatClient client = new ChatClient();
+            client.RunNode();
+        }
+    }
+}
